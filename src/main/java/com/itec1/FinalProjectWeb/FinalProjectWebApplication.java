dbee5e694ffed36e6a0347654a02bc023623cb4a
JuancaSterba <sjcexe@gmail.com>
2023-06-22 09:05:49 -0300

    se agregan los archivos de configuracion yml, y se configura el scope-test

--- src/main/java/com/itec1/FinalProjectWeb/FinalProjectWebApplication.java
+++ src/main/java/com/itec1/FinalProjectWeb/FinalProjectWebApplication.java
@@ -1,13 +1,19 @@
 package com.itec1.FinalProjectWeb;
 
+import com.itec1.FinalProjectWeb.config.SpringConfig;
+import com.itec1.FinalProjectWeb.util.ScopeUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class FinalProjectWebApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FinalProjectWebApplication.class, args);
+		ScopeUtils.calculateScopeSuffix();
+		new SpringApplicationBuilder(SpringConfig.class)
+				.registerShutdownHook(true)
+				.run(args);
 	}
 
 }
