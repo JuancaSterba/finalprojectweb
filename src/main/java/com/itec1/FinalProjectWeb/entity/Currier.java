ed582001ade9fae5b1f3c9c732a261ea608abc6e
juan Sterba <sjcexe@gmail.com>
2023-06-22 10:26:43 -0300

    se completa una entidad con notaciones para prueba

--- src/main/java/com/itec1/FinalProjectWeb/entity/Currier.java
+++ src/main/java/com/itec1/FinalProjectWeb/entity/Currier.java
@@ -1,7 +1,23 @@
 package com.itec1.FinalProjectWeb.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
 public class Currier {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
     private String cuit;
